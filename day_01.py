6a3a6b20e8eaa3fcb60b75d9869269b9706b0374
Itamar Curiel <itamarcu@gmail.com>
2018-12-01 10:03:42 +0200

    Initial commit

---
+++ day_01.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def solve(input_file_lines: List[str]) -> str:
+    
+    return str(17)
---
+++ main.py
@@ -0,0 +1,6 @@
+import day_01
+
+with open("input_01") as file:
+    input_file_lines = file.readlines()
+
+day_01.solve()
