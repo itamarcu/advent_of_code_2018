bfdb114440f5318618dd49983adf15cf422ff5a3
Itamar Curiel <itamarcu@gmail.com>
2018-12-02 13:43:24 +0200

    Day 02

---
+++ day_02.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from typing import List
+
+
+def solve_a(input_file_lines: List[str]) -> str:
+    count_2s = 0
+    count_3s = 0
+    for id1 in input_file_lines:
+        counts = defaultdict(lambda: 0)
+        for char in id1:
+            counts[char] += 1
+        if 2 in counts.values():
+            count_2s += 1
+        if 3 in counts.values():
+            count_3s += 1
+    return str(count_2s * count_3s)
+
+
+def solve_b(input_file_lines: List[str]) -> str:
+    for id1 in input_file_lines:
+        for id2 in input_file_lines:
+            diff = [bool(ord(c1)-ord(c2)) for c1, c2 in zip(id1, id2)]
+            if sum(diff) != 1:
+                continue
+            for i in range(len(diff)):
+                if diff[i]:
+                    common_string = id1[:i] + id1[i+1:]
+                    return common_string
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import day_01
+import day_02
 
-with open("input_01") as file:
+with open("input_02") as file:
     input_file_lines = file.readlines()
+    input_file_lines = [x.strip() for x in input_file_lines]
 
-print(day_01.solve(input_file_lines))
+print(day_02.solve_a(input_file_lines))
+print(day_02.solve_b(input_file_lines))
