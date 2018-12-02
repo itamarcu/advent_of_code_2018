84e14215b92a4be9a7e784315a9d1dca4e30a21f
Itamar Curiel <itamarcu@gmail.com>
2018-12-02 14:06:55 +0200

    Add day_##_template.py

---
+++ day_##_template.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def solve_a(input_file_lines: List[str]) -> str:
+    return "TODO - solve a"
+
+
+def solve_b(input_file_lines: List[str]) -> str:
+    return "TODO - solve b"
+
