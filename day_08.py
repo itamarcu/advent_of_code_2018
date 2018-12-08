e2df5914e4073d5b1bc361f9db5d7fa46a03e72e
Itamar Curiel <itamarcu@gmail.com>
2018-12-08 07:44:19 +0200

    Add iterator/generator-based solution for day 8

--- day_08.py
+++ day_08.py
@@ -1,4 +1,5 @@
 from typing import List
+from itertools import islice
 
 
 def solve_a(input_file_lines: List[str]) -> str:
@@ -47,3 +48,29 @@ def solve_b(input_file_lines: List[str]) -> str:
 
     answer, _ = recurse(0)
     return str(answer)
+
+
+def solve_a_and_b_with_iterators(input_file_lines: List[str]):
+    it = iter(int(x) for x in input_file_lines[0].split(" "))
+
+    def recurse():
+        """return (sum, value)"""
+        child_count = next(it)
+        metadata_count = next(it)
+        if child_count == 0:
+            total_value = total_sum = sum(islice(it, metadata_count))
+            return total_sum, total_value
+
+        total_sum = total_value = 0
+        child_values = []
+        for _ in range(child_count):
+            child_sum, child_value = recurse()
+            total_sum += child_sum
+            child_values.append(child_value)
+        for metadatum in islice(it, metadata_count):
+            total_value += child_values[metadatum - 1] if 0 <= metadatum - 1 <= child_count - 1 else 0
+            total_sum += metadatum
+        return total_sum, total_value
+
+    final_sum, final_value = recurse()
+    print(f"A: {final_sum}, B: {final_value}")
