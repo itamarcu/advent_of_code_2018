a4038cd6f9c7d89b22672c51de8c1c64cacdec32
Itamar Curiel <itamarcu@gmail.com>
2018-12-25 07:36:25 +0200

    Improve Day 25 (use buckets to reduce neighbor checks)

--- day_25.py
+++ day_25.py
@@ -26,8 +26,31 @@ def solve_a_slower(input_file_lines: List[str]) -> str:
     return str(constellation_count)
 
 
+def solve_a_faster(input_file_lines: List[str]) -> str:
+    def calc_bucket_key(c: Sequence[int]):
+        return sum(c)
+    all_coords = set()
+    unions = UnionFind()
+    buckets = defaultdict(lambda: set())
+    for line in input_file_lines:
+        coords = tuple([int(x) for x in line.split(",")])
+        bucket_key = calc_bucket_key(coords)
+        for other_coords in buckets[bucket_key]:
+            if dist(coords, other_coords) <= 3:
+                unions.union(coords, other_coords)
+        all_coords.add(coords)
+        for small_change in range(-3, +3 + 1):
+            buckets[bucket_key + small_change].add(coords)
+    union_counts = defaultdict(lambda: 0)
+    for coords in all_coords:
+        union_counts[unions[coords]] += 1
+
+    constellation_count = len(union_counts.keys())
+    return str(constellation_count)
+
+
 def solve_a(input_file_lines: List[str]) -> str:
-    return solve_a_slower(input_file_lines)
+    return solve_a_faster(input_file_lines)
 
 
 def solve_b(input_file_lines: List[str]) -> str:
